8f59414156b47a96e8357a436beb5709dfa8edde
Obinna <obinnaogbu80@gmail.com>
2023-01-09 13:54:15 -0800

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - function thats outputs the string
+ * @argc: integer
+ * @argv: character
+ * Return: 0
+ */
+
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
