7c0464c4596c088d6996fd25440bbaa4adc430b4
Obinna <obinnaogbu80@gmail.com>
2023-01-16 15:30:17 -0800

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,25 @@
+#ifndef dog_h
+#define dog_h
+
+
+/**
+ * struct dog - create a dictionary with different object
+ * @name: character
+ * @age:float
+ * @owner: character
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
