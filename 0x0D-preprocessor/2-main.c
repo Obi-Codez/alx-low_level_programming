913dad4ecb34a6a350d6bc3bd25c021fbd8b3eaa
Obinna <obinnaogbu80@gmail.com>
2023-01-16 14:48:38 -0800

    task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - function that prints a file its to be conpiled from
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
